d1b6bf18580039a44da7f35cb356e8acce8eb694
zpp238125-sudo <zpp238125@gmail.com>
2026-08-09 19:14:02 +0800

    Update get_stock_list.py

--- scripts/get_stock_list.py
+++ scripts/get_stock_list.py
@@ -20,44 +20,108 @@ stocks = []
 page_size = 100
 
 
+
 for page in range(1, 40):
 
 
-    params = {
-
-        "pn": page,
-
-        "pz": page_size,
-
-        "fs": "m:0+t:6,m:1+t:2",
-
-        "fields": "f12,f14"
-
-    }
+    print("正在获取第", page, "页")
 
 
-    response = requests.get(
-        url,
-        params=params,
-        headers=headers,
-        timeout=10
-    )
+    success = False
 
 
-    result = response.json()
+
+    for retry in range(3):
+
+        try:
+
+
+            params = {
+
+                "pn": page,
+
+                "pz": page_size,
+
+                "fs": "m:0+t:6,m:1+t:2",
+
+                "fields": "f12,f14"
+
+            }
+
+
+
+            response = requests.get(
+
+                url,
+
+                params=params,
+
+                headers=headers,
+
+                timeout=10
+
+            )
+
+
+
+            if not response.text:
+
+                raise Exception("返回为空")
+
+
+
+            result = response.json()
+
+
+            success = True
+
+            break
+
+
+
+        except Exception as e:
+
+
+            print(
+                "第",
+                page,
+                "页失败，重试",
+                retry+1
+            )
+
+
+            time.sleep(2)
+
+
+
+    if not success:
+
+        print(
+            "第",
+            page,
+            "页跳过"
+        )
+
+        continue
+
 
 
     data = result.get("data")
 
 
+
     if not data:
+
         break
+
 
 
     diff = data.get("diff")
 
 
+
     if not diff:
+
         break
 
 
@@ -73,28 +137,40 @@ for page in range(1, 40):
         })
 
 
+
     print(
         "已获取:",
         len(stocks)
     )
 
 
-    time.sleep(0.2)
+
+    time.sleep(1)
+
 
 
 
 with open(
+
     "data/all_stocks.json",
+
     "w",
+
     encoding="utf-8"
+
 ) as f:
 
 
     json.dump(
+
         stocks,
+
         f,
+
         ensure_ascii=False,
+
         indent=2
+
     )
 
 
@@ -103,5 +179,6 @@ print(
     "股票池最终数量:",
     len(stocks)
 )
+
 
 print("股票池更新完成")
