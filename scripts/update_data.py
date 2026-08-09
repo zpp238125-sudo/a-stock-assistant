e216b9609de87e4c991deeca4ec5c297b5196690
zpp238125-sudo <zpp238125@gmail.com>
2026-08-09 18:38:36 +0800

    Update update_data.py

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -1,10 +1,17 @@
 import json
 
-with open("data/portfolio.json", "r", encoding="utf-8") as f:
-    portfolio = json.load(f)
-
-
 print("股票助手自动更新程序启动")
 
-for stock in portfolio["stocks"]:
-    print(stock["code"], stock["name"])
+
+with open("data/all_stocks.json", "r", encoding="utf-8") as f:
+    stocks = json.load(f)
+
+
+print("股票池数量：", len(stocks))
+
+
+for stock in stocks:
+    print(
+        stock["code"],
+        stock["name"]
+    )
