110fc4b7f8845cbbdb330bd8ff87ef30272e69fa
zpp238125-sudo <zpp238125@gmail.com>
2026-08-09 18:40:17 +0800

    Update update_data.py

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -1,17 +1,65 @@
 import json
+import requests
 
-print("股票助手自动更新程序启动")
+
+print("开始获取行情")
 
 
 with open("data/all_stocks.json", "r", encoding="utf-8") as f:
     stocks = json.load(f)
 
 
-print("股票池数量：", len(stocks))
+market = {}
 
 
 for stock in stocks:
-    print(
-        stock["code"],
-        stock["name"]
+
+    code = stock["code"]
+
+    if code.startswith("6"):
+        symbol = "sh" + code
+    else:
+        symbol = "sz" + code
+
+
+    url = f"https://qt.gtimg.cn/q={symbol}"
+
+
+    try:
+        response = requests.get(url)
+
+        data = response.text
+
+
+        parts = data.split("~")
+
+
+        price = float(parts[3])
+        change = float(parts[32])
+
+
+        market[code] = {
+            "name": stock["name"],
+            "price": price,
+            "change": change
+        }
+
+
+        print(stock["name"], price, change)
+
+
+    except Exception as e:
+        print(code, "获取失败", e)
+
+
+
+with open("data/market.json", "w", encoding="utf-8") as f:
+    json.dump(
+        market,
+        f,
+        ensure_ascii=False,
+        indent=2
     )
+
+
+print("行情更新完成")
