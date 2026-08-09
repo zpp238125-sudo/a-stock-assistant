6b36b88e0a87952a9a2f53d0f502b3e5f8ff7b96
zpp238125-sudo <zpp238125@gmail.com>
2026-08-09 18:20:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
-fetch("data/portfolio.json")
-    .then(response => response.json())
-    .then(data => {
+Promise.all([
+    fetch("data/portfolio.json").then(response => response.json()),
+    fetch("data/market.json").then(response => response.json())
+])
 
-        const portfolio = document.getElementById("portfolio");
+.then(([portfolioData, marketData]) => {
 
-        portfolio.innerHTML = "";
+    const portfolio = document.getElementById("portfolio");
 
-        data.stocks.forEach(stock => {
+    portfolio.innerHTML = "";
 
-            const div = document.createElement("div");
 
-            div.innerHTML = `
-                <p>
-                <strong>${stock.name}</strong>
-                </p>
+    portfolioData.stocks.forEach(stock => {
 
-                <p>
-                股票代码：${stock.code}
-                </p>
+        const market = marketData[stock.code];
 
-                <p>
-                成本价：${stock.cost}
-                </p>
 
-                <p>
-                持股数量：${stock.shares}股
-                </p>
+        const profit =
+            ((market.price - stock.cost) * stock.shares).toFixed(2);
 
-                <hr>
-            `;
 
-            portfolio.appendChild(div);
+        const rate =
+            (((market.price - stock.cost) / stock.cost) * 100).toFixed(2);
 
-        });
+
+
+        const div = document.createElement("div");
+
+
+        div.innerHTML = `
+
+        <p>
+        <strong>${stock.name}</strong>
+        </p>
+
+        <p>
+        股票代码：${stock.code}
+        </p>
+
+
+        <p>
+        成本价：${stock.cost}
+        </p>
+
+
+        <p>
+        当前价格：${market.price}
+        </p>
+
+
+        <p>
+        今日涨跌：${market.change}%
+        </p>
+
+
+        <p>
+        持仓盈亏：
+        ${profit} 元
+        </p>
+
+
+        <p>
+        收益率：
+        ${rate}%
+        </p>
+
+        <hr>
+
+        `;
+
+
+        portfolio.appendChild(div);
 
     });
+
+});
