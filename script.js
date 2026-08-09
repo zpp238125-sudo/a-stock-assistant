7511aa79bc5e85e7b8c6d7a65c8dee18ce1694ed
zpp238125-sudo <zpp238125@gmail.com>
2026-08-09 18:16:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 fetch("data/portfolio.json")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+
+        const portfolio = document.getElementById("portfolio");
+
+        portfolio.innerHTML = "";
+
+        data.stocks.forEach(stock => {
+
+            const div = document.createElement("div");
+
+            div.innerHTML = `
+                <p>
+                <strong>${stock.name}</strong>
+                </p>
+
+                <p>
+                股票代码：${stock.code}
+                </p>
+
+                <p>
+                成本价：${stock.cost}
+                </p>
+
+                <p>
+                持股数量：${stock.shares}股
+                </p>
+
+                <hr>
+            `;
+
+            portfolio.appendChild(div);
+
+        });
+
     });
